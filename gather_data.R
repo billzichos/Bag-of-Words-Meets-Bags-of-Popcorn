53e4dc50836473ba29d77df1d92eb59e95359b84
billzichos <billzichos@hotmail.com>
2015-10-04 16:33:55 -0400

    gather data upgrade

--- gather_data.R
+++ gather_data.R
@@ -1,11 +1,11 @@
-source("~/GitHub/Get-Raw-Data/download.R")
-
 wd <- "~/GitHub/Bag-of-Words-Meets-Bags-of-Popcorn"
        
 setwd(wd)
-       
-downloadKaggle("word2vec-nlp-tutorial","unlabeledTrainData.tsv.zip")
-downloadKaggle("word2vec-nlp-tutorial","testData.tsv.zip")
-downloadKaggle("word2vec-nlp-tutorial","labeledTrainData.tsv.zip")
+
+source("~/GitHub/Get-Raw-Data/download.R")
+
+downloadSingleKaggleZip("word2vec-nlp-tutorial","unlabeledTrainData.tsv.zip", "unlabeledTrainData.tsv")
+downloadSingleKaggleZip("word2vec-nlp-tutorial","testData.tsv.zip", "testData.tsv")
+downloadSingleKaggleZip("word2vec-nlp-tutorial","labeledTrainData.tsv.zip", "labeledTrainData.tsv")
        
 ## MANUAL STEP - Extract the zip files
