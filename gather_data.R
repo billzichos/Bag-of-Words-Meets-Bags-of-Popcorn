29ff12b8700b27272fc65c1e315edc9a9a06d9aa
billzichos <billzichos@hotmail.com>
2016-05-21 17:31:21 -0400

    remove the zip files and keep directory clean.
    Use the gather_data.r file to retrieve the files for this competition.

--- gather_data.R
+++ gather_data.R
@@ -7,5 +7,3 @@ source("~/GitHub/Get-Raw-Data/download.R")
 downloadSingleKaggleZip("word2vec-nlp-tutorial","unlabeledTrainData.tsv.zip", "unlabeledTrainData.tsv")
 downloadSingleKaggleZip("word2vec-nlp-tutorial","testData.tsv.zip", "testData.tsv")
 downloadSingleKaggleZip("word2vec-nlp-tutorial","labeledTrainData.tsv.zip", "labeledTrainData.tsv")
-       
-## MANUAL STEP - Extract the zip files
